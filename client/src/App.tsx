bb6633f3e90b4d8567e6828fe66a75782a26affd
kwhong95 <kwhong95@kakao.com>
2021-01-19 20:10:19 +0900

    Routing Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { FC, lazy } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+const Join = lazy(() => import('./components/Join')); 
+const Chat = lazy(() => import('./components/Chat')); 
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      Hello World!
+      <Router>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" exact component={Chat} />
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Join.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Join: FC = () => {
+  return (
+    <div>
+      Join
+    </div>
+  )
+}
+
+export default Join;
