76ae2c9a8477cabc00b20b43ac0b0821e3c312f9
kwhong95 <kwhong95@kakao.com>
2021-01-20 21:34:00 +0900

    Server Root Settings

--- server/index.js
+++ server/index.js
@@ -23,3 +23,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // support of app/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use('/api/users', require('./routes/users'));
+
+app.use('upload', express.static('uploads'));
+
+if(process.env.NODE_ENV === "production") {
+
+  app.use(express.static("client/build"));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
