ec7a232ea08a8e94d388608d24c67547245d6f80
kwhong95 <kwhong95@kakao.com>
2021-01-20 20:44:43 +0900

    Setting config

--- server/index.js
+++ server/index.js
@@ -2,17 +2,11 @@ const app = require('express')()
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const router = require('./router');
-
 io.on('connection', socket => {
-  console.log('We have a new connection!!!');
-
-  socket.on('disconnect', () => {
-    console.log('User had Left!!!');
+  socket.on('message', ({ name, message }) => {
+    io.emit('message', { name, message })
   })
 })
-
-app.use(router);
 
 
 http.listen(4000, () => console.log('Listening on port 4000'));
